6b8de1e19d228f2eb0be1d8c3f599cd15e9eec29
Shaniz Nyawira <roselynshaniz@gmail.com>
2022-10-26 21:09:25 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(word)
+      word.downcase != @word.downcase && word.downcase.chars.sort == @word.downcase.chars.sort
+    end
+end
